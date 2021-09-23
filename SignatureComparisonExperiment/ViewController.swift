896d11d12f93b478bb71fee408360cc422eaea73
Paul Mayer <paul.mayer@knowink.com>
2021-09-23 10:12:32 -0500

    Initial Commit

---
+++ SignatureComparisonExperiment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SignatureComparisonExperiment
+//
+//  Created by Paul Mayer on 9/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
